d522660f150b4650f6e842096326c4dd5b39daf0
Yasar Selim <selimyself@MacBook-Pro-de-Yasar.local>
2021-09-09 15:26:01 +0200

    Ajout de nouveaux fichiers dans le dossier app

---
+++ app/footer.php
@@ -0,0 +1,16 @@
+<?php $today = date("Y"); ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title></title>
+  </head>
+  <body>
+  </body>
+  <footer class="footer2" id="footer">
+    <p class="myself"><?php echo "® Copyright" . " Selim Yasar " . $today ?></p>
+  </footer>
+</html>
