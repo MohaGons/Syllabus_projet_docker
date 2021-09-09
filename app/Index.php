a37c3c8194f46ce88c5efcf2973341747d3c21b8
Mohaz8 <mohammad.gons-saib@dgmail.fr>
2021-09-09 16:27:11 +0200

    indentation

--- app/Index.php
+++ app/Index.php
@@ -2,44 +2,39 @@
 include 'header.html';
 ?>
 
-
-
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   <title>Docker</title>
-</head>
-<body>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Docker</title>
+    </head>
+    <body>
+        <?php
+            require "database.php";
+            $reponse = $db->query('SELECT pseudo FROM users');
+            while ($donnees = $reponse->fetch())
+            {
+        ?>
+        <div class="mx-auto" style="width: 200px;">
+            <table class="table align-middle">
+                <thead>
+                    <tr>
+                        <th scope="col">Pseudo</th>
+                    </tr>
+                </thead>
+            <tbody>
+                <tr>
+                    <td><?php echo $donnees['pseudo'] ?></td> 
+                </tr>
+            </tbody>
+            </table> 
+        </div>
+    </body>
 </html>
-
 <?php
-require "database.php";
-$reponse = $db->query('SELECT pseudo FROM users');
-while ($donnees = $reponse->fetch())
-{
-   ?>
-   <div class="mx-auto" style="width: 200px;">
-   <table class="table align-middle">
-   <thead>
-     <tr>
-       <th scope="col">Pseudo</th>
-     </tr>
-   </thead>
-   <tbody>
-     <tr>
-       <td><?php echo $donnees['pseudo'] ?></td> 
-     </tr>
-     </tr>
-   </tbody>
- </table> 
-</div>
-</body>
- <?php
 }
 include 'footer.php';
 
--- app/createAccount.php
+++ app/createAccount.php
@@ -1,7 +1,7 @@
-	<?php include "header.html" ?>
+<?php include "header.html" ?>
 	
-	<!DOCTYPE html>
-	<html lang="fr">
+<!DOCTYPE html>
+<html lang="fr">
 	<head>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,24 +10,23 @@
 		<title>Créer un compte</title>
 	</head>
 	<body>
-
-	<form action="inscriptionController.php" method="post">
-  <div class="form-group">
-    <label for="pseudo">Pseudo</label>
-    <input type="text" class="form-control" name="pseudo" placeholder="Entrez un pseudo">
-  </div>
-  <div class="form-group">
-    <label for="password">Mot de passe</label>
-    <input type="password" class="form-control" name="password" placeholder="Entrez un mot de passe">
-  </div>
-  <div class="form-group">
-    <label for="password2">Mot de passe</label>
-    <input type="password" class="form-control" name="password2" placeholder="Entrez à nouveau votre mot de passe">
-	<small id="emailHelp" class="form-text text-muted">On ne va jamais partager votre mot de passe avec quelqu'un</small>
-  </div>
-  <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-</form>
-</body>
+		<form action="inscriptionController.php" method="post">
+  			<div class="form-group">
+    			<label for="pseudo">Pseudo</label>
+    			<input type="text" class="form-control" name="pseudo" placeholder="Entrez un pseudo">
+  			</div>
+  			<div class="form-group">
+    			<label for="password">Mot de passe</label>
+    			<input type="password" class="form-control" name="password" placeholder="Entrez un mot de passe">
+  			</div>
+  			<div class="form-group">
+    			<label for="password2">Mot de passe</label>
+    			<input type="password" class="form-control" name="password2" placeholder="Entrez à nouveau votre mot de passe">
+				<small id="emailHelp" class="form-text text-muted">On ne va jamais partager votre mot de passe avec quelqu'un</small>
+  			</div>
+  			<button name="submit" type="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</body>
 <?php include 'footer.php' ?>
 <style type="text/css">
 	.footer2{
@@ -35,4 +34,4 @@
       	bottom: 0!important;
 	}
 </style>
-	</html>
+</html>
